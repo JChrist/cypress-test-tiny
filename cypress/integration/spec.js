c537cb861952e78848161a947f2bc9207b7cad9a
jchrist <jchrist@cytech.gr>
2020-11-05 10:56:48 +0200

    added test case for recaptcha being blocked with integrity error

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,9 @@
 /// <reference types="cypress" />
 describe('page', () => {
   it('works', () => {
-    cy.visit('https://example.cypress.io')
+    cy.route2('http://localhost/index.html', {fixture: 'index.html'});
+    cy.visit('http://localhost/index.html');
+    cy.get("#button").click();
+    cy.get("#result").contains('executed');
   })
 })
